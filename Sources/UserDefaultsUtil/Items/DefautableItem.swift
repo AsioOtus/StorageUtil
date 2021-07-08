1280c47146832c6c757f7239f0c54a89fdaa0414
Asio Otus <asio.otus.verus@icloud.com>
2021-07-08 21:35:32 +0300

    Return previous commit

--- Sources/UserDefaultsUtil/Items/DefautableItem.swift
+++ Sources/UserDefaultsUtil/Items/DefautableItem.swift
@@ -7,7 +7,20 @@ open class DefaultableItem <Value: Codable>: Item<Value> {
 	
 	public init (
 		_ key: String,
-		defaultValue: @autoclosure @escaping () -> Value,
+		defaultValue: Value,
+		storage: Storage = StandardStorage.default,
+		logHandler: LogHandler? = nil,
+		queue: DispatchQueue? = nil,
+		label: String = "\(DefaultableItem.self) – \(#file):\(#line)"
+	) {
+		self.defaultValue = { defaultValue }
+		
+		super.init(key, storage: storage, logHandler: logHandler, queue: queue, label: label)
+	}
+	
+	public init (
+		_ key: String,
+		defaultValue: @escaping () -> Value,
 		storage: Storage = StandardStorage.default,
 		logHandler: LogHandler? = nil,
 		queue: DispatchQueue? = nil,
