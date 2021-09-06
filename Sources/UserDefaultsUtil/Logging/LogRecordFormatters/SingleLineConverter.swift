a8fcbfd8bf170e3b41485e65cfc515dd701e5135
Asio Otus <asio.otus.verus@icloud.com>
2021-09-06 20:40:56 +0300

    Update logging formatting

--- Sources/UserDefaultsUtil/Logging/LogRecordFormatters/SingleLineConverter.swift
+++ Sources/UserDefaultsUtil/Logging/LogRecordFormatters/SingleLineConverter.swift
@@ -4,20 +4,43 @@ public struct SingleLineLogRecordStringConverter: LogRecordStringConverter {
 	public func convert <Value> (_ record: LogRecord<Value>) -> String {
 		var messageComponents = [String]()
 		
+		if let storageLabel = record.info.storageLabel {
+			messageComponents.append(storageLabel)
+		}
+		
+		if let itemLabel = record.info.itemLabel {
+			messageComponents.append(itemLabel)
+		} else {
+			messageComponents.append("[Storage log]")
+		}
+		
 		messageComponents.append(KeyBuilder.build(prefix: record.info.storageKeyPrefix, key: record.info.key, postfix: record.details.keyPostfix))
 		messageComponents.append(record.details.operation.uppercased())
-		messageComponents.append(String(describing: record.details.existance))
+		
+		if let existance = record.details.existance {
+			messageComponents.append(existance.description)
+		} else {
+			messageComponents.append("Not exist")
+		}
 		
 		if let oldValue = record.details.oldValue {
 			messageComponents.append(String(describing: oldValue))
+		} else {
+			messageComponents.append("nil")
 		}
 		
 		if let newValue = record.details.newValue {
 			messageComponents.append(String(describing: newValue))
+		} else {
+			messageComponents.append("nil")
+		}
+		
+		if let comment = record.details.comment {
+			messageComponents.append(comment)
 		}
 		
 		if let error = record.details.error {
-			messageComponents.append(" – ERROR: \(error)")
+			messageComponents.append("ERROR: \(error)")
 		}
 		
 		let messsage = messageComponents.joined(separator: " | ")
