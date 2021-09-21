2a23f3d3f50e5d4b21524ed0674397dd0708859d
Asio Otus <asio.otus.verus@icloud.com>
2021-09-21 22:13:56 +0300

    Add public modifier to LogRecord properties

--- Sources/UserDefaultsUtil/Logging/LogRecord.swift
+++ Sources/UserDefaultsUtil/Logging/LogRecord.swift
@@ -3,38 +3,38 @@ import Foundation
 extension LogRecord {
 	public struct Info {
 		public struct ItemInfo {
-			let source: [String]
-			let label: String
-			let uuid: UUID
-			let file: String
-			let line: Int
+			public let source: [String]
+			public let label: String
+			public let uuid: UUID
+			public let file: String
+			public let line: Int
 			
 		}
 		
-		let key: String
-		let itemInfo: ItemInfo?
-		let storageLabel: String
-		let storageKeyPrefix: String?
+		public let key: String
+		public let itemInfo: ItemInfo?
+		public let storageLabel: String
+		public let storageKeyPrefix: String?
 	}
 }
 
 extension LogRecord {
 	public struct Details {
-		let operation: String
+		public let operation: String
 		
-		var newValue: Value? = nil
-		var oldValue: Value? = nil
-		var existance: Bool? = nil
-		var keyPostfix: String? = nil
+		public var newValue: Value? = nil
+		public var oldValue: Value? = nil
+		public var existance: Bool? = nil
+		public var keyPostfix: String? = nil
 		
-		var error: Error? = nil
-		var comment: String? = nil
+		public var error: Error? = nil
+		public var comment: String? = nil
 	}
 }
 
 public struct LogRecord<Value: Codable> {
-	let info: Info
-	let details: Details
+	public let info: Info
+	public let details: Details
 	
 	public func converted (_ converter: LogRecordStringConverter = SingleLineLogRecordStringConverter()) -> String {
 		converter.convert(self)
